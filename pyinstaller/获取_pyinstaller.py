38aa69f553ce872092a6081103d7617fc60dcb38
呱行次比猫 <59528590+cibimo@users.noreply.github.com>
2021-05-02 20:31:11 +0800

    Update 获取_pyinstaller.py

--- pyinstaller/获取_pyinstaller.py
+++ pyinstaller/获取_pyinstaller.py
@@ -6,7 +6,6 @@ import requests, json, os, time
 cookiePath = os.path.join(os.path.dirname(os.path.abspath(__file__)),'cookie.txt')
 if os.path.exists(cookiePath) == False:
     f = open(cookiePath,'w+')
-    f.write()
     f.close()
     input('请在生成的cookie.txt中填入你的cookie')
     input('按回车结束程序')
